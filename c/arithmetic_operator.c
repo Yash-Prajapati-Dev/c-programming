e704c305ad4353119c3a42d13ed4a0fa575c332b
yash-prajapati-dev <yashprajapati8868@gmail.com>
2026-09-06 19:16:51 +0530

    arithmetic_operator

---
+++ c/arithmetic_operator.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int x,y;
+    x = 6;
+    y = 7;
+
+    printf("\n%d",x+y);
+    printf("\n%d",x-y);
+    printf("\n%d",x*y);
+    printf("\n%d",x/y);
+
+    return 0;
+}
