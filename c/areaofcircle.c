824527d45edbf1662bd62e319072b85b409ea104
yash-prajapati-dev <yashprajapati8868@gmail.com>
2026-09-07 19:39:18 +0530

    area_of_circle

--- c/areaofcircle.c
+++ c/areaofcircle.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main(){
+
     int r = 10;
     float a = (3.14*r*r);
 
-    printf("the area of circle : %f"a );
+    printf("the area of circle :%f",a);
     
-
     return 0;
 }
