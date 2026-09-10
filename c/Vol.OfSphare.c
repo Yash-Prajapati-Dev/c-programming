0b6d30a584890f99e176d0f38fe345da3ac2bc2a
yash-prajapati-dev <yashprajapati8868@gmail.com>
2026-09-10 21:09:56 +0530

    sum_of_two_numbers

--- c/Vol.OfSphare.c
+++ c/Vol.OfSphare.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main(){
     int r = 10;
-    float a = (3.14*r*r);
+    float v = (4*3.14*r*r*r/3);
 
-    printf("the area of circle : %f"a );
+    printf("the volume of sphare : %f",v );
     
 
     return 0;
---
+++ sum_of_two_number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+int a, b, sum;
+
+printf("enter the first number : ");
+scanf("%d",&a);
+
+printf("enter the second number : ");
+scanf("%d",&b);
+
+sum = a + b;
+
+printf("the sum of two numbers is :%d",sum);
+
+
+return 0;
+
+}
+
