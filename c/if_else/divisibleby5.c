8f95f202e63184ea94c243e06c02d9c754ee00d3
yash-prajapati-dev <yashprajapati8868@gmail.com>
2026-09-21 20:42:20 +0530

    divisibleby5

---
+++ c/if_else/divisibleby5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+
+    int a;
+    printf("enter the positive integer :");
+    scanf("%d",&a);
+
+    if (a%5==0){
+        printf("the integer is divisible by 5 :");
+    }
+
+    else{
+    printf("the integer is not divisible by 5 :");
+    }
+
+        
+    
+
+    return 0;
+}
