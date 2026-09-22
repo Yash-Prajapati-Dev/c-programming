c59d7eff9d3bb5f0ba8a7f1860bd338bfc56517c
yash-prajapati-dev <yashprajapati8868@gmail.com>
2026-09-22 20:10:55 +0530

    absolutevalue

---
+++ c/if_else/absolutevalue.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+
+    int n;
+    printf("enter the integer :");
+    scanf("%d",&n);
+
+    if (n<0){
+        n =n*(-1);
+    }
+
+    printf("the absolute value is :%d",n);
+
+    
+
+    return 0;
+}
